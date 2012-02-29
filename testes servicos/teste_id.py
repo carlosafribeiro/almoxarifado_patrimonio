7a60aae45901866fc949d5d3cb8fb0645eb48735
Carlos Alexandre <carlos.alefr@gmail.com>
2012-02-29 20:20:45 -0300

    Teste Atualizado

--- testes servicos/teste_id.py
+++ testes servicos/teste_id.py
@@ -5,7 +5,7 @@ import urllib, json
 patrimonio = urllib.urlopen("http://localhost:8000/api/patrimonios/1")
 patrimonio_load = json.load(patrimonio)
 
-if patrimonio_load[0] == 'Cadeira giratória':
+if patrimonio_load[0]['descricao'] == 'Cadeira_giratoria':
 	print "OK"
 else:
 	print "Erro"
